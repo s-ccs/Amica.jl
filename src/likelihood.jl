e01066f7b4fd89f6dc5a88c656e49b73a03fdf02
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-06-14 12:25:40 +0000

    moved correct likelihoodfile

--- src/likelihood.jl
+++ src/likelihood.jl
@@ -1,18 +1,16 @@
-function calculate_LL(myAmica, iter) #lines 225 - 231
-	M = myAmica.M
-	N = myAmica.n
-	n = myAmica.n
+
+
+function calculate_LL(Lt, M, N, n) #lines 225 - 231
 	if M > 1
-		Ltmax = ones(size(myAmica.Lt))
+		Ltmax = ones(size(Lt))
 		for i in 1:N
-			Ltmax[:,i] .= maximum(myAmica.Lt[:,i])
+			Ltmax[:,i] .= maximum(Lt[:,i])
 		end
-		P = sum(exp.(myAmica.Lt-Ltmax),dims = 1)'
-		myAmica.LL[iter] = sum(Ltmax[1,:] + log.(P)) / (n*N)
+		P = sum(exp.(Lt-Ltmax),dims = 1)'
+		return sum(Ltmax[1,:] + log.(P)) / (n*N)
 	else 
-		myAmica.LL[iter] = sum(myAmica.Lt) / (n*N)
+		return sum(Lt) / (n*N)
 	end
-	return myAmica
 end
 
 function logpfun(x,rho) #taken from amica.m
@@ -31,27 +29,20 @@ function loglikelihoodMMGG(μ::AbstractVector,α::AbstractVector,ρ::AbstractVec
     return loglikelihood.(MM,x) # apply the loglikelihood to each sample individually (note the "." infront of .(MM,x))
 end
 
-function calculate_Lt!(myAmica, h)
-
-	myAmica.ldet[h] =  -log(abs(det(myAmica.A[:,:,h])))
-	myAmica.Lt[h,:] .= log(myAmica.proportions[h]) + myAmica.ldet[h]
-
-	Lt_h = myAmica.Lt[h]'
-	n = myAmica.n
-	m = myAmica.m
+function calculate_Lt!(Lt_h, Q, y, n, m, h, learnedParameters::GGParameters)
+	Lt_h = Lt_h'
 	for i in 1:n
 		for j in 1:m
-			myAmica.Q[j,:] = log.(myAmica.learnedParameters.α[j,i,h]) + 0.5*log.(myAmica.learnedParameters.β[j,i,h]) .+ logpfun(myAmica.y[i,:,j,h],myAmica.learnedParameters.ρ[j,i,h])
+			Q[j,:] = log.(learnedParameters.α[j,i,h]) + 0.5*log.(learnedParameters.β[j,i,h]) .+ logpfun(y[i,:,j,h],learnedParameters.ρ[j,i,h])
 		end
 		if m > 1
-			Qmax = ones(m,1).*maximum(myAmica.Q,dims=1);
-			Lt_h = Lt_h .+ Qmax[1,:]' .+ log.(sum(exp.(myAmica.Q - Qmax),dims = 1))
+			Qmax = ones(m,1).*maximum(Q,dims=1);
+			Lt_h = Lt_h .+ Qmax[1,:]' .+ log.(sum(exp.(Q - Qmax),dims = 1))
 		else
-			Lt_h = Lt_h .+ myAmica.Q[1,:]
+			Lt_h = Lt_h .+ Q[1,:]
 		end
 	end
 	
-	myAmica.Lt[h,:] = Lt_h
-	return myAmica
+	return Lt_h
 end
 
