116a07618ae483d8628f8856d4004ed63ec0b9a6
Alexander Lulkin <a.lulkin@gmail.com>
2023-08-08 10:01:32 +0200

    added MultiModelAmica (currently broken)

--- test/data_to_mat.jl
+++ test/data_to_mat.jl
@@ -2,35 +2,35 @@ using MAT
 using Amica
 using SignalAnalysis
 using LinearAlgebra
-using PyMNE
+#using PyMNE
 
 #_____________________________________________
-# #Generate multiple sin mixed with pink gaussian
-# t = range(0,20*π,length=100)
-# s =rand(PinkGaussian(length(t)),4)'
-# s[2,:] = sin.(t)
-# s[3,:] = sin.(2 .* t)
-# s[4,:] = sin.(10 .* t)
-# s = s .* [1,2,3,4]
-# #A = rand(size(s,1),size(s,1))
-# A = [1 1 0 0; 0 1 1 0; 0 0 1 1; 1 0 1 0]
-# x = A*s
-# #_____________________________________________
+#Generate multiple sin mixed with pink gaussian
+t = range(0,20*π,length=20000)
+s =rand(PinkGaussian(length(t)),4)'
+s[2,:] = sin.(t)
+s[3,:] = sin.(2 .* t)
+s[4,:] = sin.(10 .* t)
+#s = s .* [1,2,3,4]
+#A = rand(size(s,1),size(s,1))
+A = [1 1 0 0; 0 1 1 0; 0 0 1 1; 1 0 1 0]
+x = A*s
+#_____________________________________________
 #_____________________________________________
 #get eeg data
-data_path = pyconvert(String,@py(str(PyMNE.datasets.ssvep.data_path())))
-bids_fname =  joinpath(data_path,"sub-02","ses-01","eeg","sub-02_ses-01_task-ssvep_eeg.vhdr")
+# data_path = pyconvert(String,@py(str(PyMNE.datasets.ssvep.data_path())))
+# bids_fname =  joinpath(data_path,"sub-02","ses-01","eeg","sub-02_ses-01_task-ssvep_eeg.vhdr")
 
 
-raw = PyMNE.io.read_raw_brainvision(bids_fname, preload=true, verbose=false)
-raw.resample(128)
-raw.filter(l_freq=1, h_freq=nothing, fir_design="firwin")
-x = pyconvert(Array,raw.get_data(;units="uV"))
+# raw = PyMNE.io.read_raw_brainvision(bids_fname, preload=true, verbose=false)
+# raw.resample(128)
+# raw.filter(l_freq=1, h_freq=nothing, fir_design="firwin")
+# x = pyconvert(Array,raw.get_data(;units="uV"))
 #_____________________________________________
 
 m = 3
 M = 1
-n = 32
+n = 4
 
 #initialise random parameters (A, beta, mu) before saving them
 beta = ones(m, n, M) + 0.1 * randn(m, n, M)
@@ -50,7 +50,7 @@ for h in 1:M
     end
 end
 
-file = matopen("test/eeg_data2.mat", "w")
+file = matopen("test/breakit_data.mat", "w")
 write(file, "x", x)
 write(file, "s", s) #only save for self-mixed data
 write(file, "A", A) # "
