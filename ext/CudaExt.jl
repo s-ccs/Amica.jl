c527275e271aa74768eb2256fe3de027b530d86d
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2026-03-26 23:23:38 +0100

    ArrayKind is obsolete

--- ext/CudaExt.jl
+++ ext/CudaExt.jl
@@ -6,21 +6,19 @@ using PrecompileTools: @setup_workload, @compile_workload
 @compile_workload begin
     Amica.fit(
         SingleModelAmica,
-        zeros(Float32, 3_000, 24),
+        CUDA.zeros(Float32, 3_000, 24),
         m = 3,
         maxiter = 1,
         newt_start_iter = 0,
         show_progress = false,
-        ArrayType = CuArray,
     )
     Amica.fit(
         SingleModelAmica,
-        zeros(Float64, 3_000, 24),
+        CUDA.zeros(Float64, 3_000, 24),
         m = 3,
         maxiter = 1,
         newt_start_iter = 0,
         show_progress = false,
-        ArrayType = CuArray,
     )
 end
 
